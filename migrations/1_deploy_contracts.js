ae51a9d88b7894bc3a1c2ae8ba3c574b60456686
PavelZavadskiy <87939143+PavelZavadskiy@users.noreply.github.com>
2021-12-12 14:53:43 +0200

    Updates

--- migrations/1_deploy_contracts.js
+++ migrations/1_deploy_contracts.js
@@ -10,5 +10,7 @@ module.exports = async function (deployer) {
     await deployer.deploy(Vendor);
     let vendor = await Vendor.deployed();
     const data = vendor.contract.methods.initialize(process.env.SST_TOKEN, process.env.SST_AGREGATOR, process.env.ERC721_ADDRESS).encodeABI();
+    console.log("vendor.contract.methods.initialize");
+    console.log(data);
     await deployer.deploy(ERC1967Proxy, vendor.address, data);
 }
